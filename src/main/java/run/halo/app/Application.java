a23d44b3cf1ba549c072bc90b93914267a00d74b
nding1718 <nding1718@gmail.com>
2020-03-15 17:10:17 -0700

    add comments for the restart methods

--- src/main/java/run/halo/app/Application.java
+++ src/main/java/run/halo/app/Application.java
@@ -14,6 +14,13 @@ import run.halo.app.repository.base.BaseRepositoryImpl;
 
 /**
  * Halo main class.
+ *  SpringBootServletInitializer:
+ *  SpringBootServletInitializer is an extension of WebApplicationInitializer which runs a SpringApplication from a
+ *  traditional WAR archive deployed on a web container. This class binds Servlet, Filter and ServletContextInitializer
+ *  beans from the application context to the server.
+ *  Extending the SpringBootServletInitializer class also allows us to configure our application when it's run by the
+ *  servlet container, by overriding the configure() method.
+ *
  *
  * @author ryanwang
  * @date 2017-11-14
@@ -36,11 +43,28 @@ public class Application extends SpringBootServletInitializer {
     }
 
     /**
-     * Restart Application.
+     * Restart Application. Could be very handy if you want to
+     *  (1) Reloading config files upon changing some parameter
+     *  (2) Changing the currently active profile at runtime
+     *  (3) Re-initializing the application context for any reason
+     *
+     *
+     *  We have serveral method to restart the application
+     *      <1> Restart by creating a new Context(IOC container)
+     *          the method we are using now
+     *      <2> Actuator's restart endpoint
+     *      <3> Refresh the application context
+     *      https://www.baeldung.com/java-restart-spring-boot-app
      */
     public static void restart() {
         ApplicationArguments args = context.getBean(ApplicationArguments.class);
 
+        /**
+         * As we can see in here, it's important to recreate the context in a separate non-daemon thread —
+         * this way we prevent the JVM shutdown, triggered by the close method, from closing our application. Otherwise,
+         * our application would stop since the JVM doesn't wait for daemon threads to finish before terminating them.
+         *
+         */
         Thread thread = new Thread(() -> {
             context.close();
             context = SpringApplication.run(Application.class, args.getSourceArgs());
